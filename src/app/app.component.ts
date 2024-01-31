3140adc86201b04a0bee45db3d221d63ac4ced83
Max <tehcmanmax@gmail.com>
2024-01-31 14:13:31 +0100

    feat/content-wrapper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {CategoryNavbarComponent} from './layouts/category-navbar/category-navbar.component'
+import {FooterComponent} from './layouts/footer/footer.component';
+import {PostCardComponent} from './layouts/post-card/post-card.component';
+import {AboutComponent} from './components/about/about.component';
+import {BlogComponent} from './components/blog/blog.component';
+import {ContactComponent} from './components/contact/contact.component';
+import {HomeComponent} from './components/home/home.component';
+import {ReviewsComponent} from './components/reviews/reviews.component';
+import {ServicesComponent} from './components/services/services.component';
+import {WorkComponent} from './components/work/work.component';
+import { ContentWrapperComponent } from './utilities/content-wrapper/content-wrapper.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CategoryNavbarComponent, FooterComponent, PostCardComponent, ContentWrapperComponent,
+    AboutComponent, BlogComponent, ContactComponent, HomeComponent, ReviewsComponent, ServicesComponent, WorkComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/utilities/content-wrapper/content-wrapper.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-wrapper',
+  standalone: true,
+  imports: [],
+  templateUrl: './content-wrapper.component.html',
+  styleUrl: './content-wrapper.component.scss'
+})
+export class ContentWrapperComponent {
+
+}
