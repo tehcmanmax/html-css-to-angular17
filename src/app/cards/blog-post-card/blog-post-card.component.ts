ab985a901f4ec585daead564c9c572d0d0f5e16d
Max <tehcmanmax@gmail.com>
2024-02-01 18:29:47 +0100

    feat/add-cards

---
+++ src/app/cards/blog-post-card/blog-post-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-post-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-post-card.component.html',
+  styleUrl: './blog-post-card.component.scss'
+})
+export class BlogPostCardComponent {
+
+}
---
+++ src/app/cards/education-card/education-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-education-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './education-card.component.html',
+  styleUrl: './education-card.component.scss'
+})
+export class EducationCardComponent {
+
+}
